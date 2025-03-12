a33702b4360df8d01169cf39123c533db5333b58
Jesmin Chakma <jesminchakma39@gmail.com>
2025-03-12 11:55:46 +0600

    Dropdown navbar-menu is completed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,10 +2,18 @@ import React, { useEffect, useRef, useState } from 'react';
 import './Navbar.css'
 import { assets } from '../../assets/assets'
 import { Link, useNavigate } from 'react-router-dom';
+import { IoMenuOutline } from "react-icons/io5";
+import { IoSearch } from "react-icons/io5";
+import { IoCartOutline } from "react-icons/io5";
 
 
 
 const Navbar = () => {
+    const [open, setOpen] = useState(false);
+    if (open) {
+        console.log('Button Clicked')
+    }
+
     const navref = useRef();
     const navigate = useNavigate();
     const [menu, setMenu] = useState('menu')
@@ -28,7 +36,10 @@ const Navbar = () => {
     return (
         <div ref={navref} className='nav-wrapper'>
             <div className='navbar'>
-                <img onClick={() => navigate('/')} className='cursor-pointer logo' src={assets.logo} alt=""></img>
+                <div onClick={() => setOpen(!open)} className='md:hidden'>
+                    <IoMenuOutline className='cursor-pointer text-xl' />
+                </div>
+                <img  onClick={() => navigate('/')} className='cursor-pointer logo' src={assets.logo} alt=""></img>
                 <ul className='navbar-menu'>
                     <li onClick={() => setMenu('Home')} className={menu === 'Home' ? 'active' : ''}><Link to='/'>Home</Link></li>
                     <li onClick={() => setMenu('Menu')} className={menu === 'Menu' ? 'active' : ''}><Link to='/menu'>Menu</Link></li>
@@ -38,16 +49,30 @@ const Navbar = () => {
                 </ul>
 
                 <div className="navbar-right">
-                    <Link to='/search'> <img src={assets.search_icon} alt="" /></Link>
+                    <Link to='/search'> <IoSearch  className='navbar-right-search-img text-xl ' alt="" /></Link>
                     <div className='navbar-search-icon'>
-                        <Link to="/cart"> <img src={assets.basket_icon} alt="" ></img></Link>
+                        <Link to="/cart"> <IoCartOutline className='navbar-right-cart-img text-xl' alt="" /></Link>
                         <div className='dot'></div>
                     </div>
                     <button><Link to="/login">Login</Link></button>
                 </div>
 
+                <div className={`absolute md:hidden top-17 left-0 w-full  bg-white flex flex-col items-center gap-6 font-semibold text-lg transform transition-transform
+                    ${open ? "opacity-100 z-10" : "opacity-0"}`}
+                    style={{ transition: 'transform 0.3s ease,opacity 0.3s ease' }}>
+                    <li className='list-none w-full text-center hover:bg-sky-400 hover:text-white transition-all cursor-pointer '>Home</li>
+                    <li className='list-none w-full text-center hover:bg-sky-400 hover:text-white transition-all cursor-pointer '>Menu</li>
+                    <li className='list-none w-full text-center hover:bg-sky-400 hover:text-white transition-all cursor-pointer '>Mobile-App</li>
+                    <li className='list-none w-full text-center hover:bg-sky-400 hover:text-white transition-all cursor-pointer '>Contact</li>
+
+                </div>
+
+
+
+
 
             </div>
+
         </div>
     );
 };
