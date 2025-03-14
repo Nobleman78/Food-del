03abdfd57f9d16cfc4d73af965b2171ed04aa79d
Jesmin Chakma <jesminchakma39@gmail.com>
2025-03-14 14:06:05 +0600

    Dropdown Navigation-bar Updated

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -11,7 +11,7 @@ import Food_Logo from '../../assets/Food Logo.svg'
 
 const Navbar = () => {
     const [open, setOpen] = useState(false);
-   
+
 
     const navref = useRef();
     const navigate = useNavigate();
@@ -26,7 +26,7 @@ const Navbar = () => {
             else {
                 navref.current.classList.remove('fixed')
             }
-         
+
         })
     })
 
@@ -38,7 +38,7 @@ const Navbar = () => {
                 <div onClick={() => setOpen(!open)} className='md:hidden'>
                     <IoMenuOutline className='cursor-pointer text-xl' />
                 </div>
-                <img  onClick={() => navigate('/')} className='cursor-pointer logo' src={Food_Logo} alt=""></img>
+                <img onClick={() => navigate('/')} className='cursor-pointer logo' src={Food_Logo} alt=""></img>
                 <ul className='navbar-menu'>
                     <li onClick={() => setMenu('Home')} className={menu === 'Home' ? 'active' : ''}><Link to='/'>Home</Link></li>
                     <li onClick={() => setMenu('Menu')} className={menu === 'Menu' ? 'active' : ''}><Link to='/menu'>Menu</Link></li>
@@ -48,22 +48,22 @@ const Navbar = () => {
                 </ul>
 
                 <div className="navbar-right">
-                    <Link to='/search'> <IoSearch  className='navbar-right-search-img text-xl ' alt="" /></Link>
+                    <Link to='/search'> <IoSearch className='navbar-right-search-img text-xl ' alt="" /></Link>
                     <div className='navbar-search-icon'>
                         <Link to="/cart"> <IoCartOutline className='navbar-right-cart-img text-xl' alt="" /></Link>
                         <div className='dot'></div>
                     </div>
                     <button><Link to="/login">Login</Link></button>
-                  
+
                 </div>
 
                 <div className={`absolute md:hidden top-17 left-0 w-full  bg-white flex flex-col items-center gap-6 font-semibold text-lg transform transition-transform
                     ${open ? "opacity-100 z-10" : "opacity-0"}`}
                     style={{ transition: 'transform 0.3s ease,opacity 0.3s ease' }}>
-                    <li className='list-none w-full text-center hover:bg-sky-400 hover:text-white transition-all cursor-pointer '>Home</li>
-                    <li className='list-none w-full text-center hover:bg-sky-400 hover:text-white transition-all cursor-pointer '>Menu</li>
-                    <li className='list-none w-full text-center hover:bg-sky-400 hover:text-white transition-all cursor-pointer '>Mobile-App</li>
-                    <li className='list-none w-full text-center hover:bg-sky-400 hover:text-white transition-all cursor-pointer '>Contact</li>
+                    <Link to='/'><li className='list-none w-full text-center hover:bg-sky-400 hover:text-white transition-all cursor-pointer '>Home</li></Link>
+                    <Link to='/menu'><li className='list-none w-full text-center hover:bg-sky-400 hover:text-white transition-all cursor-pointer '>Menu</li></Link>
+                    <Link to='/mobile-app'><li className='list-none w-full text-center hover:bg-sky-400 hover:text-white transition-all cursor-pointer '>Mobile-App</li></Link>
+                    <Link to='/contact'><li className='list-none w-full text-center hover:bg-sky-400 hover:text-white transition-all cursor-pointer '>Contact</li></Link>
 
                 </div>
 
