fd74eca4a173b004e35750a4704edb0c0f4a0871
Jesmin Chakma <jesminchakma39@gmail.com>
2025-03-14 13:39:27 +0600

    Firsebase google login create-user login with user email-password added

--- src/components/Account/Login.jsx
+++ src/components/Account/Login.jsx
@@ -1,29 +1,99 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import './Login.css'
 import { FcGoogle } from "react-icons/fc";
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { GoogleAuthProvider, signInWithPopup, sendPasswordResetEmail, signInWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../../utility/firebase';
+
+
 const Login = () => {
-    const navigate = useNavigate();
-   
+
+    const [success, setSuccess] = useState(false);
+    const [errorMessage, setErrorMessage] = useState('');
+    const emailRef = useRef();
+    const provider = new GoogleAuthProvider()
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+        const form = e.target;
+        const email = form.email.value
+        const password = form.password.value
+        console.log(email, password);
+        form.reset()
+        
+
+
+        //When You want to try login functionaltiy 
+        // Email :  jiten23@gmail.com
+        // Password : 123456789
+        signInWithEmailAndPassword(auth, email, password)
+            .then(res => {
+                console.log(res.user)
+
+
+
+            })
+            .catch(error => {
+                console.log('Error', error.message)
+                setErrorMessage(error.message)
+                setSuccess(false)
+            })
+
+
+
+    }
+    const handleGoogleSignIn = () => {
+        
+        signInWithPopup(auth, provider)
+
+            .then((result) => {
+                console.log(result.user);
+                setSuccess(true)
+
+
+            })
+            .catch(error => {
+                console.log('Error', error)
+                setSuccess(false);
+
+            })
+
+    }
+    const handleForgetPassword = () => {
+        console.log('Get me email password. ', emailRef.current.value);
+        const email = emailRef.current.value;
+        if (!email) {
+            console.log('Please Provide an email address ')
+
+        }
+        else {
+            sendPasswordResetEmail(auth, email)
+                .then(() => {
+                    alert('Password Reset Email Sent!Check the gmail.')
+                })
+        }
+
+    }
+
     return (
         <div className='login-container'>
-            <form className='form-container shadow-xl '>
+            <form onSubmit={handleLogin} className='form-container shadow-xl '>
                 <h1 className='text-2xl '>Login</h1>
                 <div className='flex gap-4 items-center register-linking'>
                     <h2>New User?</h2>
-                    <p  className='text-blue-400 cursor-pointer'><Link to='/registration'>Register Now</Link></p>
+                    <p className='text-blue-400 cursor-pointer'><Link to='/registration'>Register Now</Link></p>
                 </div>
-                <p className='login-with-google flex gap-5 justify-center  items-center text-center'><FcGoogle></FcGoogle>Login With Google</p>
+                <p onClick={handleGoogleSignIn} className='login-with-google flex gap-5 justify-center  items-center text-center'><FcGoogle></FcGoogle>Login With Google</p>
                 <div className='email-container'>
                     <label className='email-label'>Email : </label>
                     <br />
-                    <input type="email" placeholder='Enter Your Email ' className='shadow-none email-input w-[100%] border-b outline-none' />
+                    <input ref={emailRef} required type="email" placeholder='Enter Your Email ' name='email' className='shadow-none email-input w-[100%] border-b outline-none' />
                 </div>
                 <br />
                 <div className='password-container'>
                     <label className='' >Password</label>
                     <br />
-                    <input type="password" placeholder='Enter Your Password' className='border-b password-input w-[100%] outline-none shadow-none' />
+                    <input required type="password" placeholder='Enter Your Password' name='password' className='border-b password-input w-[100%] outline-none shadow-none' />
                 </div>
                 <div className='items-center flex justify-between   '>
                     <div className='flex items-center gap-2'>
@@ -31,10 +101,15 @@ const Login = () => {
                         <span className='text-sm remember-me ' >Remember Me</span>
                     </div>
                     <div>
-                        <span className='text-sm '>Forgot Password</span>
+                        <a onClick={handleForgetPassword} className='text-sm cursor-pointer '>Forgot Password</a>
                     </div>
                 </div>
-                <button className='login-btn'>Login Now</button>
+                <button className='login-btn cursor-pointer'>Login Now</button>
+                {
+                    success && <p className='text-green-500'>
+                      Login Succesful
+                    </p>
+                }
             </form>
         </div>
     );
--- src/components/Account/Registration.jsx
+++ src/components/Account/Registration.jsx
@@ -2,10 +2,31 @@ import React from 'react';
 import { FcGoogle } from "react-icons/fc";
 import './Registration.css'
 import { Link } from 'react-router-dom';
+import { createUserWithEmailAndPassword } from "firebase/auth"
+import { auth } from '../../utility/firebase';
+
 const Registration = () => {
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        const form = e.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email, password);
+        form.reset();
+
+        createUserWithEmailAndPassword(auth, email, password)
+            .then(res => {
+                console.log(res.user)
+            })
+            .catch(error=>{
+                console.log(error.message)
+            })
+
+    }
     return (
         <div className='registration-container'>
-            <form className='form-container shadow-xl '>
+            <form onSubmit={handleSubmit} className='form-container shadow-xl '>
                 <h1 className='text-2xl '>Registration</h1>
                 <div className='flex gap-4 items-center register-linking'>
                     <h2>Have an account?</h2>
@@ -15,16 +36,16 @@ const Registration = () => {
                 <div className='email-container'>
                     <label className='email-label'>Email : </label>
                     <br />
-                    <input type="email" placeholder='Enter Your Email ' className='shadow-none email-input w-[100%] border-b outline-none' />
+                    <input name='email' type="email" placeholder='Enter Your Email ' className='shadow-none email-input w-[100%] border-b outline-none' />
                 </div>
                 <br />
                 <div className='password-container'>
                     <label className='' >Password</label>
                     <br />
-                    <input type="password" placeholder='Enter Your Password' className='border-b password-input w-[100%] outline-none shadow-none' />
+                    <input name='password' type="password" placeholder='Enter Your Password' className='border-b password-input w-[100%] outline-none shadow-none' />
                 </div>
-               
-                <button className='login-btn'>Signup Now</button>
+
+                <button className='login-btn cursor-pointer'>Signup Now</button>
             </form>
 
         </div>
